776612b17d512753d095b79d4fa2561616ffdeea
AOjeda06 <andres.ojeda@iesnervion.es>
2024-12-17 14:02:13 +0100

    17/12/2024

--- Tema3.2/src/boletin1/Ej17v2.java
+++ Tema3.2/src/boletin1/Ej17v2.java
@@ -8,30 +8,28 @@ public class Ej17v2 {
 
 		// Imprimimos el antes
 		System.out.println("Entrada: " + codigo);
-		// Llamamos a la funcion
+		// Llamamos a la función
 		String codigoSinComentarios = eliminarComentarios(codigo);
-		// Imprimimos el despues
+		// Imprimimos el después
 		System.out.println("Salida: " + codigoSinComentarios);
 	}
 
 	public static String eliminarComentarios(String codigo) {
 		String resultado = "";
-		int longitud = codigo.length();
 		int i = 0;
-		while (i < longitud) {
-			// Utiliza substring para extraer una subcadena de dos caracteres a partir del
-			// índice i y compara si es igual a /*. Si es así, hemos detectado el inicio de
-			// un comentario
-			if (i < longitud - 1 && codigo.substring(i, i + 2).equals("/*")) {
-				// Busca la posición del final del comentario (*/) comenzando desde el índice i
-				// + 2. Ajusta el índice i para que apunte al carácter justo después de */.
-				i = codigo.indexOf("*/", i + 2) + 2;
-			} else {
-				// Añadir el carácter actual a la cadena resultado
-				resultado += codigo.charAt(i);
-				i++;
-			}
+		int finComentario = 0;
+
+		while ((i = codigo.indexOf("/*")) >= 0) {
+			// Añadir la parte del código antes del comentario
+			resultado += codigo.substring(0, i);
+			// Encontrar el final del comentario
+			finComentario = codigo.indexOf("*/");
+			// Actualizar el índice a después del comentario
+			i = finComentario + 2;
+			// Continuar desde el final del comentario
+			codigo = codigo.substring(i);
 		}
+
 		return resultado;
 	}
 }
