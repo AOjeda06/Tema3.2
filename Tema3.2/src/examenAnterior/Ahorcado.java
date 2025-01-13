fe51d14d6d41c2ae76fbd39e919ef87813e7d3fd
AOjeda06 <andres.ojeda@iesnervion.es>
2025-01-13 13:43:23 +0100

    13/01/2025

--- Tema3.2/src/examenAnterior/Ahorcado.java
+++ Tema3.2/src/examenAnterior/Ahorcado.java
@@ -1,42 +1,63 @@
 package examenAnterior;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ahorcado {
 
+	// Array de palabras posibles para el juego
 	static String[] palabras = { "humanidad", "persona", "hombre", "mujer", "individuo", "cuerpo", "pierna", "cabeza",
 			"brazo", "familia" };
+	// Número máximo de intentos que tiene el jugador
 	static int numintentos = 7;
+	// Palabra que el jugador tiene que adivinar
 	static String palabraSecreta = "";
+	// Pista que muestra las letras acertadas y guiones bajos para las letras no
+	// acertadas
 	static StringBuilder palabraPista;
+	// Cadena que almacena las letras que el jugador ha intentado pero no están en
+	// la palabra secreta
 	static String noAcertadas = "";
+	// Escáner para leer la entrada del usuario
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		// Generar una palabra aleatoria para la palabra secreta
 		generaPalabra();
+
+		// Inicializar la pista con guiones bajos
 		palabraPista = new StringBuilder(palabraSecreta.length());
 		for (int i = 0; i < palabraSecreta.length(); i++) {
 			palabraPista.append('_');
 		}
 
+		// Mostrar la pista inicial
 		pintaPista();
+
+		// Variables para la opción seleccionada y la entrada del usuario
 		int ans;
 		String answer;
+
+		// Bucle principal del juego: continuar hasta que se adivine la palabra o se
+		// agoten los intentos
 		while (!palabraPista.toString().equalsIgnoreCase(palabraSecreta) && numintentos > 0) {
-			System.out.println();
+			System.out.println(); // Salto de línea para separar las iteraciones
+
+			// Mostrar el menú y leer la opción seleccionada hasta que sea válida
 			do {
 				ans = menu();
 			} while (ans != 1 && ans != 2);
 
+			// Procesar la opción seleccionada
 			switch (ans) {
 			case 1 -> {
+				// Opción 1: el jugador introduce una letra
 				System.out.println("Introduce la letra: ");
 				answer = scanner.next();
 				compruebaLetra(answer.charAt(0));
 			}
 			case 2 -> {
+				// Opción 2: el jugador introduce una palabra
 				System.out.println("Introduce la palabra: ");
 				answer = scanner.next();
 				compruebaPalabra(answer);
@@ -44,24 +65,30 @@ public class Ahorcado {
 			default -> System.out.println("Opción no válida");
 			}
 
+			// Mostrar la pista actualizada y el número de intentos restantes
 			pintaPista();
 			System.out.println("Intentos restantes: " + numintentos);
 		}
+
+		// Mostrar el resultado del juego
 		if (numintentos > 0) {
 			System.out.println("WINNER");
 		} else {
 			System.out.println("LOSER");
 		}
 
+		// Cerrar el escáner
 		scanner.close();
 	}
 
+	// Generar una palabra aleatoria para la palabra secreta
 	public static void generaPalabra() {
 		Random rand = new Random();
 		int random = rand.nextInt(palabras.length);
 		palabraSecreta = palabras[random];
 	}
 
+	// Mostrar el menú y devolver la opción seleccionada
 	public static int menu() {
 		int ans;
 		System.out.println("Seleccione una de las siguientes opciones: ");
@@ -71,16 +98,20 @@ public class Ahorcado {
 		return ans;
 	}
 
+	// Comprobar si la letra está en la palabra secreta
 	public static void compruebaLetra(char letra) {
 		int index = palabraSecreta.indexOf(letra);
 		boolean letraEncontrada = false;
 
+		// Buscar todas las ocurrencias de la letra en la palabra secreta
 		while (index >= 0) {
-			palabraPista.setCharAt(index, letra);
-			index = palabraSecreta.indexOf(letra, index + 1);
+			palabraPista.setCharAt(index, letra); // Actualizar la pista con la letra acertada
+			index = palabraSecreta.indexOf(letra, index + 1); // Buscar la siguiente ocurrencia
 			letraEncontrada = true;
 		}
 
+		// Si la letra no se encontró, añadirla a noAcertadas y decrementar el número de
+		// intentos
 		if (!letraEncontrada) {
 			if (noAcertadas.indexOf(letra) < 0) {
 				noAcertadas += letra + " ";
@@ -89,14 +120,16 @@ public class Ahorcado {
 		}
 	}
 
+	// Comprobar si la palabra introducida es correcta
 	public static void compruebaPalabra(String palabra) {
 		if (palabra.equalsIgnoreCase(palabraSecreta)) {
-			palabraPista = new StringBuilder(palabraSecreta);
+			palabraPista = new StringBuilder(palabraSecreta); // Actualizar la pista con la palabra acertada
 		} else {
-			numintentos--;
+			numintentos--; // Decrementar el número de intentos si la palabra es incorrecta
 		}
 	}
 
+	// Mostrar la pista y las letras no acertadas
 	public static void pintaPista() {
 		System.out.println("Letras no acertadas: " + noAcertadas);
 		for (int i = 0; i < palabraPista.length(); i++) {
--- Tema3.2/src/examenAnterior/Criptograma.java
+++ Tema3.2/src/examenAnterior/Criptograma.java
@@ -1,10 +1,86 @@
 package examenAnterior;
 
+import java.util.Random;
+
 public class Criptograma {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Array bidimensional con las letras del abecedario español
+	static char[][] abecedario = { { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' },
+			{ 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q' }, { 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' } };
 
+	// Array unidimensional con las frases para el criptograma
+	static String[] frases = {
+			"las guerras seguiran mientras el color de la piel siga siendo mas importante que el de los ojos",
+			"aprende a vivir y sabras morir bien", "cada dia sabemos mas y entendemos menos",
+			"el dinero no puede comprar la vida", "la verdadera sabiduria esta en reconocer la propia ignorancia" };
+
+	// Variables para la frase real y la frase codificada
+	static String fraseReal;
+	static String fraseCodificada;
+	static Random random = new Random();
+
+	public static void main(String[] args) {
+		desordenaAbecedario();
+	}
+
+	public static void desordenaAbecedario() {
+		// Declarar todas las variables al inicio de la función
+		int filas = abecedario.length;
+		int columnas = abecedario[0].length;
+		int i, j, row1, col1, row2, col2;
+		char temp;
+
+		// Iterar desde el último índice hasta el primero en un array unidimensional
+		// imaginario
+		for (i = filas * columnas - 1; i > 0; i--) {
+			// Generar un índice aleatorio entre 0 e i (inclusive)
+			j = random.nextInt(i + 1);
+
+			// Convertir el índice unidimensional 'i' a índices bidimensionales 'row1' y
+			// 'col1'
+			row1 = i / columnas;
+			col1 = i % columnas;
+
+			// Convertir el índice unidimensional 'j' a índices bidimensionales 'row2' y
+			// 'col2'
+			row2 = j / columnas;
+			col2 = j % columnas;
+
+			// Intercambiar los elementos en las posiciones (row1, col1) y (row2, col2) del
+			// array abecedario
+			temp = abecedario[row1][col1];
+			abecedario[row1][col1] = abecedario[row2][col2];
+			abecedario[row2][col2] = temp;
+		}
+	}
+
+	public static void eligeFrase() {
+		fraseReal = frases[random.nextInt(frases.length)];
+	}
+
+	public static void codificaFrase() {
+		for (int k = 0; k < fraseReal.length(); k++) {
+			for (int i = 0; i < abecedario.length; i++) {
+				for (int j = 0; j < abecedario[0].length; j++) {
+					if (fraseReal.charAt(k) == abecedario[i][j]) {
+						fraseCodificada += i;
+						fraseCodificada += j;
+						fraseCodificada += " ";
+					} else {
+						fraseCodificada += "  ";
+					}
+				}
+			}
+		}
+	}
+
+	public static boolean compruebaCodigo(String num, char letra) {
+		boolean correcto = false;
+		if (letra == abecedario[num.charAt(0)][num.charAt(1)]) {
+			correcto = true;
+		}
+
+		return correcto;
 	}
 
 }
