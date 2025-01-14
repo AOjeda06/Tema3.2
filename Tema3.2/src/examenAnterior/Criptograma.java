4cc5dbec5a796018719b725aaf29d01d45f9d0b1
AOjeda06 <andres.ojeda@iesnervion.es>
2025-01-14 13:41:39 +0100

    14/01/2025

--- Tema3.2/src/examenAnterior/Criptograma.java
+++ Tema3.2/src/examenAnterior/Criptograma.java
@@ -1,12 +1,13 @@
 package examenAnterior;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Criptograma {
 
 	// Array bidimensional con las letras del abecedario español
-	static char[][] abecedario = { { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' },
-			{ 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q' }, { 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' } };
+	static char[][] abecedario = { { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' },
+			{ 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q' }, { 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' } };
 
 	// Array unidimensional con las frases para el criptograma
 	static String[] frases = {
@@ -20,20 +21,51 @@ public class Criptograma {
 	static Random random = new Random();
 
 	public static void main(String[] args) {
+		// Desordenar el array del abecedario
 		desordenaAbecedario();
+		// Elegir una frase aleatoria
+		eligeFrase();
+		// Codificar la frase elegida
+		codificaFrase();
+
+		// Crear un escáner para leer la entrada del usuario
+		Scanner scanner = new Scanner(System.in);
+
+		// Bucle principal del juego: continuar hasta que las frases coincidan
+		while (!sonIguales()) {
+			// Mostrar la frase codificada
+			System.out.println("Frase codificada: " + fraseCodificada);
+			// Pedir al usuario que introduzca un código
+			System.out.print("Introduce un código (e.g., 01): ");
+			String codigo = scanner.next();
+			// Pedir al usuario que introduzca una letra
+			System.out.print("Introduce una letra: ");
+			char letra = scanner.next().charAt(0);
+
+			// Comprobar si el código introducido corresponde a la letra indicada
+			if (compruebaCodigo(codigo, letra)) {
+				System.out.println("¡Correcto!");
+			} else {
+				System.out.println("Incorrecto. Inténtalo de nuevo.");
+			}
+		}
+
+		// Felicitar al jugador una vez haya acertado la frase
+		System.out.println("¡¡ENHORABUENA!! HAS ACERTADO");
+
+		// Cerrar el escáner
+		scanner.close();
 	}
 
 	public static void desordenaAbecedario() {
-		// Declarar todas las variables al inicio de la función
+		// Obtener las dimensiones del array
 		int filas = abecedario.length;
 		int columnas = abecedario[0].length;
 		int i, j, row1, col1, row2, col2;
 		char temp;
 
-		// Iterar desde el último índice hasta el primero en un array unidimensional
-		// imaginario
+		// Desordenar el array utilizando el algoritmo de Fisher-Yates adaptado a 2D
 		for (i = filas * columnas - 1; i > 0; i--) {
-			// Generar un índice aleatorio entre 0 e i (inclusive)
 			j = random.nextInt(i + 1);
 
 			// Convertir el índice unidimensional 'i' a índices bidimensionales 'row1' y
@@ -46,8 +78,7 @@ public class Criptograma {
 			row2 = j / columnas;
 			col2 = j % columnas;
 
-			// Intercambiar los elementos en las posiciones (row1, col1) y (row2, col2) del
-			// array abecedario
+			// Intercambiar los elementos en las posiciones (row1, col1) y (row2, col2)
 			temp = abecedario[row1][col1];
 			abecedario[row1][col1] = abecedario[row2][col2];
 			abecedario[row2][col2] = temp;
@@ -55,19 +86,30 @@ public class Criptograma {
 	}
 
 	public static void eligeFrase() {
+		// Seleccionar una frase aleatoria del array 'frases'
 		fraseReal = frases[random.nextInt(frases.length)];
 	}
 
 	public static void codificaFrase() {
+		// Inicializar 'fraseCodificada' como cadena vacía
+		fraseCodificada = "";
+		// Recorrer cada carácter de 'fraseReal'
 		for (int k = 0; k < fraseReal.length(); k++) {
-			for (int i = 0; i < abecedario.length; i++) {
-				for (int j = 0; j < abecedario[0].length; j++) {
-					if (fraseReal.charAt(k) == abecedario[i][j]) {
-						fraseCodificada += i;
-						fraseCodificada += j;
-						fraseCodificada += " ";
-					} else {
-						fraseCodificada += "  ";
+			char letra = fraseReal.charAt(k);
+			if (letra == ' ') {
+				// Los espacios se representan como dos espacios en 'fraseCodificada'
+				fraseCodificada += "  ";
+			} else {
+				boolean found = false;
+				// Buscar la letra en el array 'abecedario'
+				for (int i = 0; i < abecedario.length && !found; i++) {
+					for (int j = 0; j < abecedario[i].length; j++) {
+						if (letra == abecedario[i][j]) {
+							// Añadir las coordenadas de la letra a 'fraseCodificada'
+							fraseCodificada += i + "" + j + " ";
+							found = true;
+							break;
+						}
 					}
 				}
 			}
@@ -75,12 +117,35 @@ public class Criptograma {
 	}
 
 	public static boolean compruebaCodigo(String num, char letra) {
-		boolean correcto = false;
-		if (letra == abecedario[num.charAt(0)][num.charAt(1)]) {
-			correcto = true;
+		// Convertir el código (e.g., "01") a índices de fila y columna
+		int fila = Character.getNumericValue(num.charAt(0));
+		int columna = Character.getNumericValue(num.charAt(1));
+		// Comprobar si la letra en 'abecedario[fila][columna]' coincide con la letra
+		// introducida
+		if (fila < abecedario.length && columna < abecedario[0].length && abecedario[fila][columna] == letra) {
+			// Reemplazar el código por la letra en 'fraseCodificada'
+			String codigoCompleto = num + " ";
+			fraseCodificada = fraseCodificada.replace(codigoCompleto, letra + " ");
+			return true;
 		}
+		return false;
+	}
 
-		return correcto;
+	public static boolean sonIguales() {
+		// Separar 'fraseCodificada' en partes por los espacios
+		String[] codificadaArray = fraseCodificada.trim().split(" +");
+		StringBuilder fraseCodificadaLimpia = new StringBuilder();
+		// Quitar espacios vacíos en las partes codificadas
+		for (String parte : codificadaArray) {
+			if (!parte.equals("")) {
+				fraseCodificadaLimpia.append(parte);
+			}
+		}
+		// Quitar los espacios de 'fraseReal'
+		String fraseRealSinEspacios = fraseReal.replace(" ", "");
+		// Comprobar si las versiones limpias y sin espacios de 'fraseCodificada' y
+		// 'fraseReal' son iguales
+		return fraseCodificadaLimpia.toString().equalsIgnoreCase(fraseRealSinEspacios);
 	}
 
 }
